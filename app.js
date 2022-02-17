1210b2f381adee651f0fc780e8ddbe388772ff56
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-02-17 14:49:44 -0500

    change get route

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/:nombre', (req, res) => {
+app.get('/makers/:nombre', (req, res) => {
   let nombre=req.params.nombre;
   nombre=nombre[0].toUpperCase() + nombre.slice(1);
   res.send(`<h1>Hola ${nombre}!</h1>`);
 });
+
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
