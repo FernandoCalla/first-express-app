fe0ca54dac1735a6f995835697440e8a8d8bf3ca
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-02-22 17:37:33 -0500

    add pug template

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.get('/', (req, res) => {
-  let letras=''
-  for(let i=0;i<50;i++){
-    const numeroVisual=i+1;
-    if(numeroVisual%2===0){
-      letras+=`<p>${numeroVisual} Soy Par!</p>`
-    }
-    else{
-      letras+=`<p>${numeroVisual} Soy Impar!</p>`
-    }
+  // let letras=''
+  // for(let i=0;i<50;i++){
+  //   const numeroVisual=i+1;
+  //   if(numeroVisual%2===0){
+  //     letras+=`<p>${numeroVisual} Soy Par!</p>`
+  //   }
+  //   else{
+  //     letras+=`<p>${numeroVisual} Soy Impar!</p>`
+  //   }
+  // }    
+  // res.send(letras);
+  const texts = [];
+  for (let i=1; i <=50; i++) {
+    texts.push(`${i} Soy ${(i % 2 == 0 ? "Par" : "Impar")}!`);
   }
-  // for(let i=0;i<50;i++){    
-  res.send(letras);
-  
-  // }
+
+  res.render("index", { texts: texts });
 });
 
 
